1dd57ffe4d791eb4661a3d7ac95add748fbc875b
Divyaansh Mertia <divyaanshmertia@gmail.com>
2021-06-09 19:26:25 +0530

    Assignment-6 09-06-2021

---
+++ Assignments/Assignments/Assignment-6/script.js
@@ -0,0 +1,73 @@
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator__keys')
+const display = calculator.querySelector('.calculator__display')
+const operatorKeys = keys.querySelectorAll('[data-type="operator"]')
+
+keys.addEventListener('click', event => {
+  if (!event.target.closest('button')) return
+
+  const key = event.target
+  const keyValue = key.textContent
+  const displayValue = display.textContent
+  const { type } = key.dataset
+  const { previousKeyType } = calculator.dataset
+
+  if (type === 'number') {
+    if (
+      displayValue === '0' ||
+      previousKeyType === 'operator'
+    ) {
+      display.textContent = keyValue
+    } else {
+      display.textContent = displayValue + keyValue
+    }
+  }
+
+  if (type === 'operator') {
+    operatorKeys.forEach(el => { el.dataset.state = '' })
+    key.dataset.state = 'selected'
+
+    calculator.dataset.firstNumber = displayValue
+    calculator.dataset.operator = key.dataset.key
+  }
+
+  if (type === 'equal') {
+    
+    const firstNumber = calculator.dataset.firstNumber
+    const operator = calculator.dataset.operator
+    const secondNumber = displayValue
+    display.textContent = calculate(firstNumber, operator, secondNumber)
+  }
+
+  if (type === 'clear') {
+    display.textContent = '0'
+    delete calculator.dataset.firstNumber
+    delete calculator.dataset.operator
+  }
+
+  calculator.dataset.previousKeyType = type
+})
+
+function calculate (firstNumber, operator, secondNumber) {
+  firstNumber = parseFloat(firstNumber)
+  secondNumber = parseFloat(secondNumber)
+
+  if (operator === 'plus') return firstNumber + secondNumber
+  if (operator === 'minus') return firstNumber - secondNumber
+  if (operator === 'times') return firstNumber * secondNumber
+  if (operator === 'divide') return firstNumber / secondNumber
+}
+
+
+function clearCalculator () {
+ 
+  const clearKey = document.querySelector('[data-type="clear"]')
+  clearKey.click()
+
+  
+  operatorKeys.forEach(key => { key.dataset.state = '' })
+}
+
+
+
+
